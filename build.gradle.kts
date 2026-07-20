377f5d55dff4916e56843123750f3aec25b25aec
Ivan Perevozchikov <ivanpv607@gmail.com>
2026-07-20 22:35:22 +0500

    init: project setup

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("java")
+}
+
+group = "com.serverest"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // REST Assured + Jackson
+    implementation("io.rest-assured:rest-assured:5.5.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.3")
+
+
+    // JUnit 5
+    testImplementation(platform("org.junit:junit-bom:5.11.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // AssertJ
+    testImplementation("org.assertj:assertj-core:3.26.3")
+
+    // JSON Schema Validation
+    testImplementation("io.rest-assured:json-schema-validator:5.5.0")
+
+    // Allure
+    implementation("io.qameta.allure:allure-java-commons:2.29.1")
+    testImplementation("io.qameta.allure:allure-rest-assured:2.29.1")
+
+    // DataFaker
+    testImplementation("net.datafaker:datafaker:2.4.3")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok:1.18.46")
+    annotationProcessor("org.projectlombok:lombok:1.18.46")
+    testCompileOnly("org.projectlombok:lombok:1.18.46")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.46")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "serve-rest-automation-tests"
