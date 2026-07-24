495c218ce71fcfbfeae4a09cec51294639824f82
Ivan Perevozchikov <ivanpv607@gmail.com>
2026-07-24 21:12:33 +0500

    ci: fix pipeline and add Allure plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("io.qameta.allure") version "4.0.2"
 }
 
 group = "com.serverest"
@@ -15,34 +16,32 @@ repositories {
     mavenCentral()
 }
 
+allure {
+    version = "2.29.1"
+    report {
+        reportDir.set(layout.buildDirectory)
+    }
+}
+
 dependencies {
-    // REST Assured + Jackson
     implementation("io.rest-assured:rest-assured:5.5.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
 
-
-    // JUnit 5
     testImplementation(platform("org.junit:junit-bom:5.11.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    // AssertJ
     testImplementation("org.assertj:assertj-core:3.26.3")
 
-    // JSON Schema Validation
     testImplementation("io.rest-assured:json-schema-validator:5.5.0")
 
-    // Allure
     implementation("io.qameta.allure:allure-java-commons:2.29.1")
     testImplementation("io.qameta.allure:allure-rest-assured:2.29.1")
 
-    // DataFaker
     testImplementation("net.datafaker:datafaker:2.4.3")
 
-    // JJWT
     testImplementation("io.jsonwebtoken:jjwt:0.12.6")
 
-    // Lombok
     compileOnly("org.projectlombok:lombok:1.18.46")
     annotationProcessor("org.projectlombok:lombok:1.18.46")
     testCompileOnly("org.projectlombok:lombok:1.18.46")
@@ -51,4 +50,7 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    doFirst {
+        delete("build/allure-results")
+    }
 }
